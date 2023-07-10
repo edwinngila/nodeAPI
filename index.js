325d6706c690a872544afabde8230cb82f2ca3b5
Edwin ngila kyalo <edwinngilakyalo@gmail.com>
2023-07-10 11:57:03 +0300

    04/07/2023

--- index.js
+++ index.js
@@ -23,5 +23,4 @@ app.use((err,req,res,next)=>{
         }
     })
 })
-app.listen(process.env.port||4000,()=>{console.log("now listening on : http://localhost:4000");
-});
+app.listen(process.env.port);
